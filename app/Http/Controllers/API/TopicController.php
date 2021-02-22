4553774bf34a9b64d780fd2ae9391ac87839db13
Killian P <KillianPiccerelle@sfr.fr>
2021-02-22 13:59:23 +0100

    First methods topic

---
+++ app/Http/Controllers/API/TopicController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Categorie;
+use App\Http\Controllers\Controller;
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class TopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $topics = Topic::latest('created_at')->simplepaginate(8);
+        $categories = Categorie::all() ;
+
+        return response()->json([
+            'topics' => $topics,
+            'categories' => $categories
+        ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Categorie::all() ;
+
+        return response()->json([
+        'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:5',
+            'content' => 'required|min:10',
+            'categorie_id'=>'required',
+        ]);
+
+        $topic = new Topic();
+        $topic->content = $request->input('content');
+        $topic->title = $request->input('title');
+        $topic->categorie_id = $request->input('categorie_id');
+        $topic->user_id = auth()->user()->id;
+
+        return response()->json($topic->save());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $topic = Topic::find($id);
+        $topic->user;
+        $topic->comments;
+
+        return response()->json([
+            'topic' => $topic
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $topic = Topic::find($id);
+
+        return response()->json([
+            'topic' => $topic
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (){
    return \App\Models\User::all();
 });
+
+
+
+
+
+/**
+ *TOPIC ROUTES
+ */
+route::get('topics',[\App\Http\Controllers\API\TopicController::class,'index']);
+route::get('topics/create',[\App\Http\Controllers\API\TopicController::class,'create']);
+route::post('topics/store',[\App\Http\Controllers\API\TopicController::class,'store']);
+route::get('topics/{id}/show',[\App\Http\Controllers\API\TopicController::class,'show']);
+route::get('topics/{id}/edit',[\App\Http\Controllers\API\TopicController::class,'edit']);
+
+
